9ffcbb2e272dd18d0d9f078b4c28b498e498f3d6
Varun Ajmera <varun.ajmera@nokia.com>
2012-10-19 17:04:46 -0400

    adding Netty stuff

---
+++ netty/LearningNetty/src/sample/TimeServerHandler.java
@@ -0,0 +1,46 @@
+package sample;
+
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.buffer.ChannelBuffers;
+import org.jboss.netty.channel.Channel;
+import org.jboss.netty.channel.ChannelFuture;
+import org.jboss.netty.channel.ChannelFutureListener;
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.ChannelStateEvent;
+import org.jboss.netty.channel.ExceptionEvent;
+import org.jboss.netty.channel.SimpleChannelHandler;
+
+public class TimeServerHandler extends SimpleChannelHandler {
+	
+	public void channelConnected(ChannelHandlerContext ctx , ChannelStateEvent e){
+		Channel ch = e.getChannel();
+		
+		ChannelBuffer time = ChannelBuffers.buffer(4);
+		
+		time.writeInt((int) (System.currentTimeMillis()/1000));
+		
+		ChannelFuture f = ch.write(time);
+		
+		f.addListener(new ChannelFutureListener() {
+			
+			@Override
+			public void operationComplete(ChannelFuture future) {
+				Channel ch = future.getChannel();
+				
+				ch.close();
+	
+			}
+		});
+		
+	}
+	public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e){
+		
+		e.getCause().printStackTrace();
+		e.getChannel().close()
+		
+	}
+		
+		
+	}
+
+}
